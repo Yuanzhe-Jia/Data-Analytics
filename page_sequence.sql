f0b150a88a1ba5d4fef621780bf759d723c439ae
Yuanzhe Jia <104203996+Yuanzhe-Jia@users.noreply.github.com>
2024-01-02 18:12:51 +0800

    Add files via upload

---
+++ page_sequence.sql
@@ -0,0 +1,54 @@
+with
+dataset as (
+select
+	clientId,
+    eventTimeMs,
+    eventName,
+    leadInFrame(eventTimeMs, 1) over (partition by clientId order by eventTimeMs rows between unbounded preceding and unbounded following) as timeBehind,
+    leadInFrame(eventName, 1) over (partition by clientId order by eventTimeMs rows between unbounded preceding and unbounded following) as eventBehind
+from 
+ 	`default`.app_events_dist
+where
+ 	customerId = 1960183601
+ 	and platform = 'web'
+	and date(eventTimeMs) between '2023-12-27' and '2023-12-31'	
+	and eventName like '%conviva_%'
+),
+
+dataset_1 as (
+select 
+	clientId, eventTimeMs, eventName, timeBehind,
+	if(eventBehind = '', 'null', eventBehind) as eventBehind,
+	dateDiff('second', eventTimeMs, timeBehind) as timeDiff
+from dataset
+where eventName = 'conviva_page_view'
+),
+
+dataset_2 as (
+select 
+	eventBehind,
+	count(1) as cnt,
+	round(count(1) / sum(count(1)) over(partition by 1), 3) as pct
+from dataset_1
+group by 1
+),
+
+dataset_3 as (
+select 
+	case when timeDiff = 0 then '0'
+		 when timeDiff > 0 and timeDiff <= 1 then '0-1'
+		 when timeDiff > 1 and timeDiff <= 2 then '1-2'
+		 when timeDiff > 2 and timeDiff <= 3 then '2-3'
+		 when timeDiff > 3 and timeDiff <= 5 then '3-5'
+		 when timeDiff > 5 and timeDiff <= 10 then '5-10'
+		 else '10+' end as tag,
+	count(1) as cnt,
+	round(count(1) / sum(count(1)) over(partition by 1), 3) as pct
+from dataset_1
+where timeDiff >= 0
+group by 1
+)
+
+select * from dataset_2 order by 2 desc
+
+--select * from dataset_3 order by 1
